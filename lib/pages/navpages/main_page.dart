791e7ef94324b295269c46530f8bfbd5b1cec635
Steven Ndaye <stevenndaye@gmail.com>
2022-04-22 16:29:11 +0200

    Added bottom navigation bar

---
+++ lib/pages/navpages/main_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cubit_app/pages/navpages/bar_item_page.dart';
+import 'package:flutter_cubit_app/pages/navpages/home_page.dart';
+import 'package:flutter_cubit_app/pages/navpages/my_page.dart';
+import 'package:flutter_cubit_app/pages/navpages/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  int currentIndex = 0;
+  List pages = [HomePage(), BarItemPage(), SearchPage(), MyPage()];
+
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedFontSize: 0,
+        selectedFontSize: 0,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        onTap: onTap,
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.black54,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        elevation: 0,
+        items: [
+          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.apps)),
+          BottomNavigationBarItem(
+              label: "Bar", icon: Icon(Icons.bar_chart_sharp)),
+          BottomNavigationBarItem(label: "Search", icon: Icon(Icons.search)),
+          BottomNavigationBarItem(label: "My", icon: Icon(Icons.person))
+        ],
+      ),
+    );
+  }
+}
+
+
+// class MainPage extends StatelessWidget {
+//   MainPage({Key? key}) : super(key: key);
+//
+//   int currentIndex = 0;
+//   List pages = [HomePage(), BarItemPage(), SearchPage(), MyPage()];
+// //
+//   void onTap(int index) {
+//     setState(() {
+//       currentIndex = index;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: pages[0],
+//       bottomNavigationBar: BottomNavigationBar(
+//         onTap: onTap,
+//         currentIndex: currentIndex,
+//         selectedItemColor: Colors.black54,
+//         unselectedItemColor: Colors.grey.withOpacity(0.5),
+//         showSelectedLabels: false,
+//         showUnselectedLabels: false,
+//         elevation: 0,
+//         items: [
+//           BottomNavigationBarItem(label: "Home", icon: Icon(Icons.apps)),
+//           BottomNavigationBarItem(
+//               label: "Bar", icon: Icon(Icons.bar_chart_sharp)),
+//           BottomNavigationBarItem(label: "Search", icon: Icon(Icons.search)),
+//           BottomNavigationBarItem(label: "My", icon: Icon(Icons.person))
+//         ],
+//       ),
+//     );
+//   }
+// }
