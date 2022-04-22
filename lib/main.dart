82db2087be32756aee1b2ca450ed66972fa95547
Steven Ndaye <stevenndaye@gmail.com>
2022-04-22 14:54:33 +0200

    Completer the builder of a customer slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_cubit_app/welcome_page.dart';
+import 'package:flutter_cubit_app/pages/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
